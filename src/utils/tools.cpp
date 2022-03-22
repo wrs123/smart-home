505760e633021a8904a1229644b74a97a92f8d68
ofg <1309870573@qq.com>
2022-03-22 15:08:00 +0800

    add: 添加网络时间方法

--- src/utils/tools.cpp
+++ src/utils/tools.cpp
@@ -9,12 +9,33 @@
  * 
  */
 #include "tools.h"
+#include <Arduino.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+
 
 /**
  * @brief 
  * 获取网络时间
  */
 void getNetworkTime(void){
-    
 
-}
+    DynamicJsonDocument doc(1024);
+    HttpClient http;
+    String resBuff;
+
+    http.begin(TIME_API_URL);
+    int httpCode = http.get();
+
+    if(httpCode > 0){
+        if(httpCode == HTTP_CODE_OK){
+            resBuff = http.getString();
+            Serial.println(resBuff);
+        }
+    }else{
+
+    }
+    http.end();
+    
+}   
--- src/utils/tools.h
+++ src/utils/tools.h
@@ -11,6 +11,10 @@
 #ifndef TOOLS_H
 #define TOOLS_H
 
+#ifndef TIME_API_URL
+    #define TIME_API_URL "http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp"
+#endif //TIME_API_URL
+
 
 void getNetworkTime(void);
 
